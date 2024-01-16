91b9ee3ce2d66339d448ee9076812f35e03af0c9
kasla <kasla@DESKTOP-SR937O1>
2024-01-15 19:47:40 -0800

    MainPage move to Views folder

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -4,24 +4,10 @@ namespace WalletApp.Views
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
             ((BaseViewModel)BindingContext).ContentPage = this;
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 
